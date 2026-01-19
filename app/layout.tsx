19fc9353962df9f142b100ed4eba2672d575664c
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2026-01-19 19:55:40 +0530

    Moved FloatingDock to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { FloatingDock } from "@/components/ui/floating-dock";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        {/* Floating Dock Wrapper */}
+        <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50">
+          <FloatingDock />
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,6 @@ import { FloatingDock } from "@/components/ui/floating-dock";
 
 export default function Home() {
   return <>
-    <div className="h-screen w-screen flex justify-center">
-      <div className="flex items-end space-y-10">
-        <FloatingDock />
-      </div>
-    </div>
+    
   </>;
 }
